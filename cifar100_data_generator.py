b028b314bd0502f9a81b9a0816a012846cc61911
mdabbah <34287255+mdabbah@users.noreply.github.com>
2019-06-12 19:54:40 +0300

    code clean-up and comment

--- cifar100_data_generator.py
+++ cifar100_data_generator.py
@@ -1,8 +1,9 @@
+import keras
 from keras.datasets import cifar100
 from keras.utils import np_utils
 import numpy as np
 from keras_preprocessing.image import ImageDataGenerator
-from  keras.utils import to_categorical
+from itertools import count
 
 # load data
 (X_train, y_train), (X_test, y_test) = cifar100.load_data()
@@ -16,6 +17,45 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
+
+def rearrange_samples(X_train, Y_train, nb_classes):
+    """rearranges samples so it guaranties that 20% of pairs in each
+    batch is of the same class"""
+
+    # rearrange the dataset so it has the samples in the following order
+    # AAAA...BBBB...CCCC... where A,B,C ... are our classes
+    X_train = X_train[np.argsort(y_train, axis=0), :, :, :]
+    Y_train = Y_train[np.argsort(y_train, axis=0), :]
+
+    x_train_copy = np.copy(np.squeeze(X_train))
+    y_train_copy = np.copy(np.squeeze(Y_train))
+    class_size = 500
+    num_samples_per_class_in_bach = 20
+    x_train_copy = np.reshape(x_train_copy, [nb_classes, class_size, 32, 32, 3])
+    y_train_copy = np.reshape(y_train_copy, [nb_classes, class_size, nb_classes])
+
+    randomize_class_order = np.random.permutation(nb_classes)
+    x_train_copy = x_train_copy[randomize_class_order, :, :, :, :]
+    y_train_copy = y_train_copy[randomize_class_order, :, :]
+
+    randomize_samples_order = np.random.permutation(class_size)
+    x_train_copy = x_train_copy[:, randomize_samples_order, :, :, :]
+    y_train_copy = y_train_copy[:, randomize_samples_order, :]
+
+    x_train_copy = np.reshape(x_train_copy, [-1, num_samples_per_class_in_bach, 32, 32, 3])
+    y_train_copy = np.reshape(y_train_copy, [-1, num_samples_per_class_in_bach, nb_classes])
+    randomize_20 = np.random.permutation(x_train_copy.shape[0])
+    x_train_copy = x_train_copy[randomize_20, :, :, :, :]
+    y_train_copy = y_train_copy[randomize_20, :, :]
+    X_train = np.reshape(x_train_copy, [-1, 32, 32, 3])
+    Y_train = np.reshape(y_train_copy, [-1, nb_classes])
+
+    return X_train, Y_train
+
+
+np.random.seed(0)
+X_train, Y_train = rearrange_samples(X_train, Y_train, nb_classes)
+
 # subtract mean and normalize
 mean_image = np.mean(X_train, axis=0)
 X_train -= mean_image
@@ -23,12 +63,7 @@ X_test -= mean_image
 X_train /= 128.
 X_test /= 128.
 
-# split data for validataion
-np.random.seed(0)
-perm = np.random.permutation(X_train.shape[0])
-X_train = X_train[perm, :, :, :]
-Y_train = Y_train[perm, :]
-
+# split data for validation
 num_training_samples = int(X_train.shape[0]*0.9)
 X_valid = X_train[num_training_samples:, :, :, :]
 X_train = X_train[:num_training_samples, :, :, :]
@@ -53,3 +88,34 @@ datagen = ImageDataGenerator(
 # Compute quantities required for featurewise normalization
 # (std, mean, and principal components if ZCA whitening is applied).
 datagen.fit(X_train)
+
+
+
+# class MYGenerator(keras.utils.Sequence):
+#
+#     def __init__(self, train_folder, labels_file, batch_size, preprocess_fun_name):
+#
+#         file_names = os.listdir(train_folder)
+#         image_file_names = [os.path.join(train_folder, im_file) for im_file in file_names]
+#         file_names = [os.path.splitext(os.path.basename(file))[0] for file in file_names]
+#         labels = pd.read_csv(labels_file)
+#         labels = [labels.loc[labels['id'] == file_names[idx]]['breed_id'] for idx in range(len(file_names))]
+#         labels = [int(round(l)) for l in labels]
+#         labels = np.array(labels, np.int)
+#         assert len(labels) == len(file_names), "not all images have labels"
+#
+#         self.image_file_names, self.labels = image_file_names, labels
+#         self.batch_size = batch_size
+#         self.preprocess_fun = preprocess_fun_name
+#         print("created a generator from {:}".format(train_folder))
+#
+#     def __len__(self):
+#         return np.int(np.ceil(len(self.image_file_names) / float(self.batch_size)))
+#
+#     def __getitem__(self, idx):
+#         # batch_x = self.image_file_names[idx * self.batch_size:(idx + 1) * self.batch_size]
+#         # batch_y = self.labels[idx * self.batch_size:(idx + 1) * self.batch_size]
+#         #
+#         # return np.array([
+#         #     resize(preprocess_fun(imread(file_name)), (224, 224))
+#         #     for file_name in batch_x]), labels
--- train_distance_based.py
+++ train_distance_based.py
@@ -1,5 +1,5 @@
 import distance_classifier
-import cifar100_data_generator
+import cifar100_data_generator as data_genetator
 import numpy as np
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import ReduceLROnPlateau
@@ -7,70 +7,97 @@ from keras.callbacks import CSVLogger
 from keras.callbacks import EarlyStopping
 import keras.backend as K
 import tensorflow as tf
-from keras.applications.vgg16 import VGG16
-import sys
+from keras.optimizers import Nadam
+# from keras.applications.vgg16 import VGG16
 
 
 def distance_loss(encodeing_layer):
 
     def loss(y_true, y_pred):
-        emebddings = K.squeeze(encodeing_layer.output, 1)
-        emebddings = K.squeeze(emebddings, 1)
-        norms = tf.norm(K.repeat(emebddings, batch_size) - tf.transpose(K.repeat(emebddings, batch_size), [1, 0, 2]), axis=2)
-        total_loss = K.categorical_crossentropy(y_true, y_pred)
+        embeddings = K.squeeze(encodeing_layer.output, 1)
+        embeddings = K.squeeze(embeddings, 1)
 
-        print_op_pred = tf.print("total loss: ", total_loss)
+        # print the embeddings
+        # print_op_embedd = tf.print("embeddings: ", embeddings)
 
-        y_eq = tf.matmul(y_true, tf.transpose(y_true)) > 0
-        # print_op = tf.print("eq: ", tf.reduce_sum(tf.matmul(y_true, tf.transpose(y_true)))/10000)
+        # calculate the embeddings distance from each other in the current batch
+        norms = tf.norm(K.repeat(embeddings, batch_size) - tf.transpose(K.repeat(embeddings, batch_size), [1, 0, 2]), axis=2)
 
-        distance_loss_eq = tf.reduce_sum(tf.boolean_mask(norms, y_eq))
+        # the original classification loss
+        total_loss = tf.reduce_sum(K.categorical_crossentropy(y_true, y_pred))
+        print_op_pred = tf.print("classification loss: ", total_loss)  # print it
+
+        # boolean matrix s.t. in entry (i,j) is 1 iff label_i == label_j
+        y_eq = tf.matmul(y_true, tf.transpose(y_true))
+        print_op = tf.print("eq. pairs percentage: ", tf.reduce_sum(y_eq)/batch_size**2)  # print how manny pairs are equal
+
+        # the proposed distance loss
+        distance_loss_eq = tf.reduce_sum(tf.boolean_mask(norms, y_eq))  # loss for pairs with the same label
+        distance_loss_diff = tf.reduce_sum(tf.maximum(0., distance_margin - tf.boolean_mask(norms, 1-y_eq)))  # loss for pairs with different label
+        # print them
         print_op2 = tf.print("loss equals: ", distance_loss_eq)
-        distance_loss_diff = tf.reduce_sum(tf.maximum(0., distance_margin - tf.boolean_mask(norms, tf.logical_not(y_eq))))
         print_op3 = tf.print("loss diff: ", distance_loss_diff)
 
-        with tf.control_dependencies([print_op2, print_op_pred, print_op3]):
-            return total_loss + (distance_loss_eq + distance_loss_diff)*distance_loss_coeff
+        # total_loss += (distance_loss_eq + distance_loss_diff) * distance_loss_coeff
+        return total_loss
+        # with tf.control_dependencies([print_op, print_op2, print_op_pred, print_op3]):
+        #     return total_loss + (distance_loss_eq + distance_loss_diff) * distance_loss_coeff
 
     return loss
 
 
 if __name__ == '__main__':
 
-    weights_file = 'distance_classifier_{epoch: 03d}_{vall_acc:.3f}_{vall_loss:.3f}_{train_acc:.3f}_{train_loss:.3f}.h5'
+    # file name & after epoch callbacks
+    weights_file = './results/distance_classifiers/' \
+                   'distance_classifier_{epoch: 03d}_{val_acc:.3f}_{val_loss:.3f}_{acc:.3f}_{loss:.3f}.h5'
     lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0,
                                    patience=5, min_lr=0.5e-6)
     early_stopper = EarlyStopping(min_delta=0.001, patience=20)
     csv_logger = CSVLogger('distance_classifier-CIFAR-10.csv')
     model_checkpoint = ModelCheckpoint(weights_file, monitor='val_acc', save_best_only=True,
                                        save_weights_only=True, mode='auto')
+    my_callbacks = [csv_logger, model_checkpoint] #, lr_reducer, early_stopper]
 
-    my_callbacks = [csv_logger, model_checkpoint, lr_reducer, early_stopper]
+    # training constants
     batch_size = 100
     distance_margin = 25
     distance_loss_coeff = 0.2
 
-    # y_train = np.expand_dims(cifar100_data_generator.Y_train, axis=1)
-    # y_train = np.expand_dims(y_train, axis=1)
-    my_generator = cifar100_data_generator.datagen.flow(cifar100_data_generator.X_train,
-                                                        cifar100_data_generator.Y_train,
-                                                        batch_size=batch_size)
+    my__training_generator = data_genetator.datagen.flow(data_genetator.X_train,
+                                                         data_genetator.Y_train,
+                                                         batch_size=batch_size, shuffle=True)
 
-    c100_classifier = distance_classifier.DistanceClassifier((32, 32, 3), num_classes=100)
+    my__validation_generator = data_genetator.datagen.flow(data_genetator.X_valid,
+                                                           data_genetator.Y_valid,
+                                                           batch_size=batch_size, shuffle=True)
 
-    validation_data = cifar100_data_generator.X_valid, cifar100_data_generator.Y_valid
+    my_classifier = distance_classifier.DistanceClassifier((32, 32, 3), num_classes=100)
 
-    num_training_xsamples_per_epoch = cifar100_data_generator.X_train.shape[0] // batch_size
-    num_validation_xsamples_per_epoch = cifar100_data_generator.X_valid.shape[0] // batch_size
+    validation_data = data_genetator.X_valid, data_genetator.Y_valid
 
-    encoder = c100_classifier.get_layer('embedding')
+    num_training_xsamples_per_epoch = data_genetator.X_train.shape[0] // batch_size
+    num_validation_xsamples_per_epoch = data_genetator.X_valid.shape[0] // batch_size
 
-    c100_classifier.compile(optimizer='sgd', loss=distance_loss(encoder), metrics=['accuracy'])
-    c100_classifier.fit_generator(my_generator,
-                                  steps_per_epoch=num_training_xsamples_per_epoch,
-                                  callbacks=my_callbacks,
-                                  validation_data=validation_data,
-                                  validation_steps=num_validation_xsamples_per_epoch,
-                                  workers=1,
-                                  use_multiprocessing=False)
+    encoder = my_classifier.get_layer('embedding')
+
+    optimizer = Nadam(lr=1e-4)
+    my_classifier.compile(optimizer='Nadam', loss=distance_loss(encoder), metrics=['accuracy'])
+    my_classifier.fit_generator(my__training_generator,
+                                epochs=150,
+                                steps_per_epoch=num_training_xsamples_per_epoch,
+                                callbacks=my_callbacks,
+                                validation_data=my__validation_generator,
+                                validation_steps=num_validation_xsamples_per_epoch,
+                                workers=1,
+                                use_multiprocessing=False)
+
+    test_generator = data_genetator.datagen.flow(data_genetator.X_test,
+                                                           data_genetator.Y_test,
+                                                           batch_size=batch_size, shuffle=True)
+
+    loss, acc = my_classifier.evaluate_generator(test_generator,
+                                                    steps=data_genetator.X_test.shape[0]//batch_size)
+
+    print(f'test acc {acc}, test loss {loss}')
 
