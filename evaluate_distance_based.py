6f9b0ec57083ecd006b521b58b11114a7f7f857f
mdabbah <34287255+mdabbah@users.noreply.github.com>
2019-06-16 19:51:00 +0300

    updated evaluation script

--- evaluate_distance_based.py
+++ evaluate_distance_based.py
@@ -1,5 +1,10 @@
 from keras.models import load_model
+from keras.optimizers import SGD
+from sklearn.metrics import classification_report, roc_auc_score
 
+from train_distance_based import distance_loss
+import distance_classifier
+import cifar100_data_generator as data_genetator
 
 
 def distance_score(x_embedding):
@@ -16,10 +21,24 @@ def distance_score(x_embedding):
 def pickle_embeddings(model, x_train, y_train, pickle_name):
     """pickles the embeddings outputted by the model from its embedding layer into
     a tuple (x_embed , y) where x_embed is num_train_samples X embedding_length"""
-    model.pr
+    pass
 
 
 if __name__ == '__main__':
 
     model_name = './results/distance_classifiers/distance_classifier_ 126_0.627_2.389_0.759_1.609.h5'
-    model = load_model(model_name)
+    exp_name = 'distance trained model, SR predicted'
+    batch_size = 100
+    my_classifier = distance_classifier.DistanceClassifier((32, 32, 3), num_classes=100)
+    my_classifier.load_weights(model_name)
+    optimizer = SGD(lr=1e-2, momentum=0.9)
+    encoder = my_classifier.get_layer('embedding')
+    my_classifier.compile(optimizer=optimizer, loss=distance_loss(encoder), metrics=['accuracy'])
+
+    test_generator = data_genetator.MYGenerator(data_type='test', batch_size=batch_size, shuffle=True)
+    y_pred = my_classifier.predict_generator(test_generator)
+    y_true = test_generator.labels
+    print(classification_report(y_true, y_pred))
+
+    auc = roc_auc_score(y_true, y_pred)
+    print(f'auc score for {exp_name}')
