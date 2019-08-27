fixed an important bug in risk coverage calculation

Resnet18_cifar_100.py
@@ -8,6 +8,8 @@ from keras.utils import np_utils
 import numpy as np
 from keras_preprocessing.image import ImageDataGenerator
 from keras.losses import categorical_crossentropy
+from sklearn.model_selection import train_test_split
+
 
 # load data
 (X_train, y_train), (X_test, y_test) = cifar100.load_data()
@@ -22,19 +24,32 @@ X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
 
+def normalize(X_train, X_test):
+    '''
+    This function normalize inputs for zero mean and unit variance
+    Args:
+        X_train: np array of train samples, axis 0 is samples.
+        X_test: np array of test/validation samples, axis 0 is samples.
+    Returns:
+        A tuple (X_train, X_test), Normalized version of the data.
+    '''
+    mean = np.mean(X_train, axis=(0, 1, 2, 3))
+    std = np.std(X_train, axis=(0, 1, 2, 3))
+    X_train = (X_train - mean) / (std + 1e-7)
+    X_test = (X_test - mean) / (std + 1e-7)
+    return X_train, X_test
+
+
+X_train, X_test = normalize(X_train, X_test)
+
+
 np.random.seed(0)
 
-# CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
-# CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
-
-mean_image = np.mean(X_train, axis=0)
-X_train -= mean_image
-X_test -= mean_image
-X_train /= 128.
-X_test /= 128.
-
-
 # split data for validation
+X_train, X_valid, Y_train, Y_valid = train_test_split(X_train,
+                                                      Y_train,
+                                                      test_size=0.1,
+                                                      random_state=1)
 # num_training_samples = int(X_train.shape[0]*0.9)
 # X_valid = X_train[num_training_samples:, :, :, :]
 # X_train = X_train[:num_training_samples, :, :, :]
@@ -50,7 +65,7 @@ datagen = ImageDataGenerator(
     featurewise_std_normalization=False,  # divide inputs by std of the dataset
     samplewise_std_normalization=False,  # divide each input by its std
     zca_whitening=True,  # apply ZCA whitening
-    rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+    rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
     width_shift_range=0.1,  # randomly shift images horizontally
     height_shift_range=0.1,  # randomly shift images vertically
     horizontal_flip=True,  # randomly flip images

risk_evaluation.py
@@ -28,7 +28,6 @@ def calc_selective_risk(coverage: float, test_losses, uncertainty_on_validation,
      sample i
     :return: selective risk
     """
-    uncertainty_on_validation = np.sort(uncertainty_on_validation)
     th = np.percentile(uncertainty_on_validation, coverage * 100)
 
     loss_on_covered = np.mean(test_losses[uncertainty_on_test < th])
@@ -39,7 +38,7 @@ def calc_selective_risk(coverage: float, test_losses, uncertainty_on_validation,
           f'test loss on covered ,{loss_on_covered :.6f}, '
           f'risk is: ,{risk :.6f}')
 
-    return np.mean(test_losses[uncertainty_on_test < th]) / np.mean(uncertainty_on_test < th)
+    return np.mean(test_losses[uncertainty_on_test < th])
 
 
 def turn_on_dropout(model: Model, new_rate = 0.05):
@@ -144,9 +143,9 @@ def distance_predictions(my_regressor, test_generator, training_generator):
 if __name__ == '__main__':
 
     # general params
-    data_set = 'concrete_strength'
+    data_set = 'concrete_strength'  # concrete_strength ,  facial_key_points
     arch = 'facial_key_points_arc'
-    uncertainty_metric = 'min_distance'  # options 'min_distance' , 'MC_dropout_std'
+    uncertainty_metric = 'MC_dropout_std'  # options 'min_distance' , 'MC_dropout_std'
     batch_size = 32
     input_size = None
     mc_dropout_rate = K.variable(value=0)
@@ -190,8 +189,8 @@ if __name__ == '__main__':
         my_regressor = model(input_size, 1, mc_dropout_rate=mc_dropout_rate)
         regressor_weights_path = r'./results/regression/l1_smooth_losss_simple_FCN_concrete_strength/' \
                        r'l1_smooth_loss_simple_FCN_ 659_3.177_3.137_27.02343_ 24.50994.h5'
-        regressor_weights_path = r'./results/regression/distance_by_x_encodings_simple_FCN_concrete_strength/' \
-                       r'distance_by_x_encoding_simple_FCN_ 573_32.042_31.230_26.25158_ 27.36713.h5'
+        # regressor_weights_path = r'./results/regression/distance_by_x_encodings_simple_FCN_concrete_strength/' \
+        #                r'distance_by_x_encoding_simple_FCN_ 573_32.042_31.230_26.25158_ 27.36713.h5'
 
     # load weights
     my_regressor.name = os.path.split(regressor_weights_path)[-1][:-3]